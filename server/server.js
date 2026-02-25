94c5d41ca9a036701072aad8f9cc19c5926d4c6d
Frankline Macharia <frankmasha6@gmail.com>
2026-02-25 22:31:35 +0300

    update

--- server/server.js
+++ server/server.js
@@ -137,5 +137,8 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
-server.listen(PORT, () => console.log(`Echo server running on http://localhost:${PORT}`));
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
